907445febe97e417c2fdd4b548aed2d3f15476ce
Téo Trompier <teo.trompier@gmail.com>
2025-09-04 09:15:46 +0200

    Commit #2

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('TP CIEL');
+/* *********************** Serveur Web *************************** */
+
+// Importer le module Express
+var express = require('express');
+
+var exp = express();
+
+var port = 80;
+
+exp.use(express.static(__dirname + '/www'));
+
+// Réponse pour la racine "/"
+exp.get('/', function (req, res) {
+    console.log('Réponse envoyée à un client');
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+// Gestion des erreurs
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur Express');
+});
+
+// Lancer le serveur
+exp.listen(port, function () {
+    console.log('Serveur en ecoute sur le port ' + port);
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,18 @@
+// eslint.config.cjs
+const globals = require("globals");
+const pluginJS = require("@eslint/js");
+
 module.exports = [
     {
+        languageOptions: {
+            globals: globals.browser,
+        },
         rules: {
-            // Add rules here.
-        }
-    }
+            "indent": ["warn", 4],
+            "linebreak-style": ["error", "windows"],
+            "quotes": ["error", "single"],
+            "semi": ["error", "always"]
+        },
+    },
+    pluginJS.configs.recommended,
 ];
